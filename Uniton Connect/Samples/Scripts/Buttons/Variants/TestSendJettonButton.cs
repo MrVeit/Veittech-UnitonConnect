c8ba7f07dd6d8d914578f2eddfd8802e808859ba
Mr_Veit <Misster.veit@gmail.com>
2024-12-18 03:48:50 +1200

    MInor fix

--- Uniton Connect/Samples/Scripts/Buttons/Variants/TestSendJettonButton.cs
+++ Uniton Connect/Samples/Scripts/Buttons/Variants/TestSendJettonButton.cs
@@ -7,12 +7,12 @@ namespace UnitonConnect.Core.Demo
     {
         public sealed override void OnClick()
         {
-            decimal gasFee = (decimal)0.1f;
+            decimal gasFee = (decimal)0.025f;
             var gasFeeInNano = $"{gasFee.ToNanoton()}";
 
-            var jettonWalletContract = "EQCm_De6XYgW5eh1Btl6XflRi2f5xycbEc1bXlQhznMhPvJZ"; // MY USDT JETTON WALLET
-            var payload = "te6cckEBAQEAVQAApg+KfqUAAAAAAAAAADB6EggBmIXSoQ8FMzUOUUqXFbQci+" +
-                "0muieb5E7kkN/Zk1UMX+sAKb8N7pdiBbl6HUG2Xpd+VGLZ/nHJxsRzVteVCHOcyE+ASJk/CA==";
+            var jettonWalletContract = "0:a6fc37ba5d8816e5e87506d97a5df9518b67f9c7271b11cd5b5e5421ce73213e"; // MY USDT JETTON WALLET
+            var payload = "te6cckEBAQEAWQAArg+KfqUABil5I2tQKDA9CQgBmIXSoQ8FMzUOUUqXFbQci+0muieb5E7kkN/Zk1U" +
+                "MX+sAKb8N7pdiBbl6HUG2Xpd+VGLZ/nHJxsRzVteVCHOcyE+IdzWUAM0MYCY=";
 
             TonConnectBridge.SendJetton(jettonWalletContract, gasFeeInNano, 
                 payload, (transactionHash) =>
